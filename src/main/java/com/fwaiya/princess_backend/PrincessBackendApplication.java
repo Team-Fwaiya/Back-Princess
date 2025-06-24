fb7544ec44850dda2505e06c1a535143db69c639
JuJu-0225 <alice2338@naver.com>
2025-06-24 20:19:34 +0900

    스프링부트 초기 설정

---
+++ src/main/java/com/fwaiya/princess_backend/PrincessBackendApplication.java
@@ -0,0 +1,13 @@
+package com.fwaiya.princess_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrincessBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrincessBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fwaiya/princess_backend/PrincessBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fwaiya.princess_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrincessBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
