df8e23303a45e24a9e9a3ecd192acd4fa1244df6
yaaan7 <yaaan7@kau.kr>
2025-07-09 23:54:48 +0900

    필요없는 import 삭제2

--- src/main/java/com/fwaiya/princess_backend/controller/UserController.java
+++ src/main/java/com/fwaiya/princess_backend/controller/UserController.java
@@ -8,8 +8,6 @@ import com.fwaiya.princess_backend.global.constant.ProfileImageConstants;
 import com.fwaiya.princess_backend.login.jwt.CustomUserDetails;
 import com.fwaiya.princess_backend.service.UserService;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
